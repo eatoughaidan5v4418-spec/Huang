6dcc2b58923b1e0ae34cddb31ed002ab4a3070d4
aidanhdk <3315866635@qq.com>
2026-02-13 15:53:41 +0800

    Fix metro.config.js - remove react-native-web mapping that causes mobile crash

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs'];
-
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  'react-native': path.resolve(__dirname, 'node_modules/react-native-web'),
-};
 
 module.exports = config;
